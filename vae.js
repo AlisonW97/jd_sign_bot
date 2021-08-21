8ef082330b136f962d716adddb5e41a9c0e69f13
AlisonW97 <69679720+AlisonW97@users.noreply.github.com>
2021-08-21 14:06:19 +0800

    Update vae.js

--- vae.js
+++ vae.js
@@ -13,10 +13,10 @@ const headers = {
 'Content-Type' : `application/x-www-form-urlencoded`,
 'Accept-Encoding' : `gzip, deflate, br`,
 'Host' : `api1-xusong.taihe.com`,
-'User-Agent' : `VaeHome/2.4.1 (iPhone; iOS 13.5; Scale/2.00)`,
+'User-Agent' : `VaeHome/2.5.1 (iPhone; iOS 13.5; Scale/2.00)`,
 'Accept-Language' : `zh-Hans-CN;q=1`
 };
-const body = `q=Du4edFQxtrMs2PGGMouKQYPav2RdZHmZZcsBD0/5aWKeLm1B%2BqJ3oEiLm3TKthnJSasxe9Bim1A9qIfJjGli9diTQuT9wHhEcGCFqpQnYpP01iTj/ZvBVtSM6keaFBj/JYHN8jJXrBN/v%2Bgv0v/pyeN5ZIxaEmBxWPyjShhNR%2BYaflyINEfxVKc/rx4uhfAs2PwSnWdUuqNBvCf6Ct0XId7rsCUU4zX9adTTbN1Ij26Z90cGRQ7RIx8q/vueOTqQnpfJ81iQ0PW%2BtQaNkYS2mbF3FbRONyz8lWhqk6jvfPJP91YFAKlZDFn%2BEQ1nDUV3titVAzi6BpMP4uXX8m7BWYLbXDqFCAYiiDsrjrUvK3YuD1OTvnK7tg9%2BdmLrM15aazjSq9VUmFKmP%2BF6NotbD7uddERMlQ8lodWXwhjiARBl%2B0qpBRUZnz%2B1haQ041nNjIAEF1ZYhq4VfpLgQLa7kwjVbZLjjobXb8MICqxleiOX1o88eLGp9Gnkm3LrTwvYT3dr5S3RwGYazreWhlq4gHq9PFNNvQ6wipzxndPtKLu34eRplqEDWelQiJKSBzZEJPiMj4eXLSdOXb5uH7bm85s/TTFUqHcQ2FRIhr1Nwk%2BAny2zeAMVKQ8nygAPwjqtIu5CbKZafU71phU9jBndORyzr16Hk32JLWXE7taPpJuakKLVWa9PZkGbwOt07OzsYb3djhWxM8IGRIDzICxqP714hYeNhaQYDHmFNxS8nKCu257tzOCS29%2BnGd9xscggUcSfG2UsPV1Yn8a73cA84/U3WNhh2E5hgre/jl80bbphN7trHN6/n435gEZ3rrv%2BahopxZgqdQUipQ/FguXanA%3D%3D`;
+const body = `q=821G1tnPpcsGmHJNexU4hLkabeyz4V04fAjUT0QYETxUe2/iMeOBaGJ/yTiuOamZZSVzrWAiPQmD38bssDSCSXL/ICFaHJ0n5gj0KGmenhbV8a9P5MBE3P0e1kI/hKZaz5BUwvTWkeLNAkuiDcCSf8bszCAsB/y/nIoCYLrYxjlKYlzblvyPCLcYDO8gCTslgzyBbpeSGw/5Vxww9eD%2BJHw9ml%2Bejw20f2/kgVchiPpVoKBIsXfrzBHjfHqEUufnMRkHZF6nK58GsFa/EXbUE3KHCTxwVzh%2BMW8Zh0fN4eV4fNNypbWV6UCXUzWUUfASodvBWBm9IBqIceBRzH1BxqJKnYnQe5/aqwC/gp6VVYO/yfJyJ93gD7ow1Wk9HqGnuda9r%2BePs9qVToQk%2BWxAKwaStC0Sv%2BGFoJT46v0qOL9SWoxqs0ekg0uFrA9vqFx9ju3WJp1HqNhohiEO14bOsb3mQbVTqwcBLADFMhW/Kb7eA0Lv1c%2B3Sdab5etf7iWBINZq2O/jyDpSWFqWnE/RBXs/EJSzEX6Ri4IKzJg8UBvm487RMWEiBiemywuQEnV3dKf1Q%2B4rNi2K/3JWoXO3We5Oekvnvc9f1ZMYsFhyZikfKDOUs7ty8ys2ZZX9k268E3kohxUQDMctSund1pe1I%2BgnwOgG%2Be76zFCdFlC5MZ8pSvEUehokRJGHayuB1PR/b4EwMlZ7qzTD%2Bp4tvA1YgYYxAcalANVvC0qJUw9GYjHRB0r3cXxsQPEKZ74l%2BqyrB/d2VMjHiW8GqfSqxHVCnDt3p/aTjZaWDmR/UqMZMKTnQSIxT7wVuyb87iQFvYJeJIB112haoPbIxsWaW1dgMQ%3D%3D`;
 
 const myRequest = {
     url: url,
